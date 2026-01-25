6ed5f3c2412ae608db78bfb401d7b0d6a49e6d6e
ajpsyk <ajpsyk@gmail.com>
2026-01-24 20:03:40 -0800

    moved button to masthead, added observer for lazy loaded elements, added service worker listeners for debbugging plus some refactoring

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
+let tabStates = {};
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  const isPageComplete = changeInfo.status === "complete";
-  const isVideoPage = tab.url && tab.url.includes("youtube.com/watch")
-  
-  if (isPageComplete && isVideoPage) {
-    chrome.tabs.sendMessage(tabId, { message: "TAB_UPDATE"});
-  }
+    const isPageComplete = changeInfo.status === "complete";
+    const isYoutube = tab.url && tab.url.includes("youtube.com");
+
+    if (!isYoutube) delete tabStates[tabId];
+    if (tabStates[tabId] && isPageComplete && isYoutube) chrome.tabs.sendMessage(tabId, { message: "YT_PAGE_LOADED", url: tab.url});
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === "SCRIPTS_LOADED" && sender.tab) {
+        tabStates[sender.tab.id] = true;
+    }
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+    delete tabStates[tabId];
 });
--- contentScript.js
+++ contentScript.js
@@ -1,53 +1,95 @@
 (() => {
-    let hideRelatedBtn;
 
     chrome.runtime.onMessage.addListener((obj) => {
-        if (obj.message === "TAB_UPDATE") {
-            newVideoLoaded();
-        }
+        if (obj.message === "YT_PAGE_LOADED") youtubePageLoaded(obj.url);
     });
 
-    const newVideoLoaded = () => {
-        let btn = document.querySelector(".hide-related-btn");
-        if (!btn) {
-            const secondary = document.querySelector("ytd-watch-flexy #secondary");
-            if (!secondary) {
-                setTimeout(newVideoLoaded, 500);
-                return;
-            }
+    const youtubePageLoaded = async (url) => {
 
-            hideRelatedBtn = document.createElement("button");
-            hideRelatedBtn.classList.add(
-                "yt-spec-button-shape-next",
-                "yt-spec-button-shape-next--tonal",
-                "yt-spec-button-shape-next--mono",
-                "yt-spec-button-shape-next--size-m",
-                "yt-spec-button-shape-next--icon-button",
-                "hide-related-btn"
-            );
-            hideRelatedBtn.title = "Click to hide related content.";
-            hideRelatedBtn.style.marginBottom = "10px"; 
+        await waitForElement("ytd-masthead #center");
+        await waitForElement("ytd-watch-flexy #secondary");
 
-            const img = document.createElement("img");
-            img.src = chrome.runtime.getURL("assets/visibility.svg");
-
-            hideRelatedBtn.appendChild(img);
-
-            chrome.storage.local.get("btnIsHidden", (data) => {
-                const related = document.getElementById("related");
-                related.hidden = data.btnIsHidden || false;
-                updateButtonIcon(related.hidden);
-            });
-
-            secondary.prepend(hideRelatedBtn);
-            
-            hideRelatedBtn.addEventListener("click", hideRelatedBtnEventHandler);
-        }
+        const buttonExists = document.querySelector("#hide-related-button");
+        !buttonExists ? createButton() : updatePageState();
+    
     }
 
-    const hideRelatedBtnEventHandler = () => {
+    const waitForElement = (selector) => {
+        return new Promise(resolve => {
+            const el = document.querySelector(selector);
+            if (el) return resolve();
+
+            const observer = new MutationObserver(() => {
+                const el = document.querySelector(selector);
+                if (el) {
+                    resolve();
+                    observer.disconnect(); 
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    };
+
+    const createButton = () => {
+        console.log("There is no button. Creating button...");
+        
+        const buttonContainer = document.createElement("div");
+        buttonContainer.id = "hide-related-button";
+        buttonContainer.classList.add(
+            "style-scope", "ytd-masthead"
+        );
+        buttonContainer.style.marginLeft = "12px";
+        buttonContainer.addEventListener("click", hideRelatedEventHandler);
+
+        const button = document.createElement("button");
+        button.classList.add(
+            "yt-spec-button-shape-next",
+            "yt-spec-button-shape-next--text",
+            "yt-spec-button-shape-next--mono",
+            "yt-spec-button-shape-next--tonal",
+            "yt-spec-button-shape-next--size-m", 
+            "yt-spec-button-shape-next--icon-only-default",
+            "yt-spec-button-shape-next--enable-backdrop-filter-experiment"
+        );
+        button.ariaLabel = "Hide related content";
+        button.ariaDisabled = "false";
+        buttonContainer.appendChild(button);
+
+        const img = document.createElement("img");
+        button.appendChild(img);
+
+        const mastheadCenter = document.querySelector("ytd-masthead #center");
+        mastheadCenter.appendChild(buttonContainer);
+       
+        updatePageState();
+    }
+
+    const updatePageState = () => {
+        console.log(`Button is created. Getting page state.`);
+        chrome.storage.local.get("btnIsHidden", (data) => {
+            data.btnIsHidden === undefined ? console.log("Button state not set. Defaulting to visible state.") : console.log("Related is visible: ", !data.btnIsHidden);
+            const related = document.getElementById("related");
+            const isHidden = data.btnIsHidden || false;
+            related.hidden = isHidden;
+            chrome.storage.local.set({"btnIsHidden": isHidden});
+            updateButtonIcon(isHidden);
+        });
+    }
+
+    const hideRelatedEventHandler = () => {
         const related = document.getElementById("related");
         const isHidden = !related.hidden;
+        console.log("Button Clicked!")
+
+        if (isHidden) {
+            console.log("Hiding related content.");
+        } else {
+            console.log("Showing related content")
+        }
 
         related.hidden = isHidden;
         chrome.storage.local.set({"btnIsHidden": isHidden});
@@ -55,12 +97,16 @@
     }
 
     const updateButtonIcon = (isHidden) => {
-        const buttonImg = hideRelatedBtn.querySelector("img");
+        if (isHidden) {
+            console.log("Button using visibility off icon.");
+        } else {
+            console.log("Button using visibility on icon.")
+        }
+        const buttonImg = document.querySelector("#hide-related-button").querySelector("img");
         buttonImg.src = chrome.runtime.getURL(
             isHidden ? "assets/visibility_off.svg" : "assets/visibility.svg"
         );
     }
 
-    newVideoLoaded();
-    
+    chrome.runtime.sendMessage("SCRIPTS_LOADED");
 })();
